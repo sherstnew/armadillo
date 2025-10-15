be747f31ba1bc8b612bf3eec87f66b8b8bb9678c
dragon.starwings@gmail.com <sherstnev.denis.v@gmail.com>
2025-10-15 10:59:54 +0300

    fix 2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'standalone',
-};
+const nextConfig = {
+  // output: 'standalone', // пока закомментируйте для теста
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
 
 export default nextConfig;
